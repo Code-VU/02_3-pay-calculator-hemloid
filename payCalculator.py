6d0500794441fbe9334718124caa9278859d7cd7
Austin Loyd <austin@Austins-Air.lan>
2023-05-02 07:03:51 -0500

    converted to fload, printed total pay

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,15 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hours = float(input("Enter Hours:"))
+    hourly_rate = float(input('Enter Rate:'))
+    
+    total_pay = (hours * hourly_rate)
+
+    print(total_pay)
+    
+
+
 
     # end assignment
 
